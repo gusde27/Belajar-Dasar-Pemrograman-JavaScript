fddd00ac4d587fd3c0199a79272b2912536fb738
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-04-23 12:31:40 +0800

    Kuis Coding Update

--- Error Hendling/KuisCoding.js
+++ Error Hendling/KuisCoding.js
@@ -45,49 +45,51 @@
 
 // TODO 1
 class ValidationError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "ValidationError";
-    }
+  constructor(message) {
+      super(message);
+      this.name = "ValidationError";
+  }
 }
 
 // TODO 2
 const validateNumberInput = (a,b,c) =>{
-	if ( typeof a != "number" ) {
-        throw new ValidationError("Argumen pertama harus number");
-    }
-  if ( typeof b != "number" ) {
-        throw new ValidationError("Argumen kedua harus number");
-    }
-  if ( typeof c != "number" ) {
-        throw new ValidationError("Argumen ketiga harus number");
-    }
+if ( typeof a != "number" ) {
+      throw new ValidationError("Argumen pertama harus number");
+  }
+if ( typeof b != "number" ) {
+      throw new ValidationError("Argumen kedua harus number");
+  }
+if ( typeof c != "number" ) {
+      throw new ValidationError("Argumen ketiga harus number");
+  }
 }
 
 
 const detectTriangle = (a, b, c) => {
-  // TODO 3
-  try {
-     validateNumberInput(a, b, c);
-  } catch (error) {
-     validateNumberInput(a, b, c);
-  }
+// TODO 3
+try {
+   validateNumberInput(a, b, c);
+    if (a === b && b === c) {
+      return 'Segitiga sama sisi';
+    }
+    if (a === b || a === c || b === c) {
+      return 'Segitiga sama kaki';
+    }
+    return 'Segitiga sembarang';
+  
+} catch (error) {
+   if (error instanceof ValidationError) {
+     return `${error.message}`;
+   } else {
+     return `${error.stack}`;
+   }
+}
 
-  if (a === b && b === c) {
-    //return 'Segitiga sama sisi';
-    console.log('Segitiga sama sisi');
-  }
-
-  if (a === b || a === c || b === c) {
-    //return 'Segitiga sama kaki';
-    console.log('Segitiga sama kaki');
-  }
-
-  console.log('Segitiga sembarang');
-  //return 'Segitiga sembarang';
 };
 
+console.log(detectTriangle(10,'ss',90));
+
 /**
- * Hiraukan kode di bawah ini
- */
+* Hiraukan kode di bawah ini
+*/
 module.exports = { ValidationError, validateNumberInput, detectTriangle };
