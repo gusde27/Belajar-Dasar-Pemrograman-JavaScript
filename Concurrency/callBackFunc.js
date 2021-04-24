f18ba86d4086090fc2f04a2dfe68fe62c141972e
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-04-24 13:11:08 +0800

    Update CallBack Function

--- Concurrency/callBackFunc.js
+++ Concurrency/callBackFunc.js
@@ -1,11 +1,24 @@
-const orderCoffee = () => {
+// const orderCoffee = () => {
+//     let coffee = null;
+//     console.log("Sedang membuat kopi, silakan tunggu...");
+//     setTimeout(() => {
+//         coffee = "Kopi sudah jadi!";
+//     }, 3000);
+//     return coffee;
+// }
+
+// const coffee = orderCoffee();
+// console.log(coffee); GAGAL Mengambil nilai Null
+
+const orderCoffee = callback => {
     let coffee = null;
     console.log("Sedang membuat kopi, silakan tunggu...");
     setTimeout(() => {
         coffee = "Kopi sudah jadi!";
-    }, 3000);
+        callback(coffee);
+    }, 10000);
     return coffee;
 }
- 
-const coffee = orderCoffee();
-console.log(coffee);
+
+orderCoffee(coffee => { console.log(coffee); });
+
