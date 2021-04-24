100703ccfa5dd7ccd8a333993463a10d0056ca19
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-04-24 13:21:28 +0800

    Update Promise

--- Concurrency/promise1.js
+++ Concurrency/promise1.js
@@ -18,3 +18,21 @@ console.log(coffeePromise);
 /* output
 Promise { 'Kopi berhasil dibuat' }
 */
+
+/*
+Executor function memiliki dua parameter, yaitu resolve dan reject 
+yang berupa fungsi. Berikut penjelasan detailnya:
+
+resolve() adalah parameter pertama pada executor function. Parameter 
+ini merupakan fungsi yang dapat menerima satu parameter. Biasanya kita
+gunakan untuk mengirimkan data ketika promise berhasil dilakukan. Ketika
+fungsi ini terpanggil, kondisi Promise akan berubah dari pending menjadi
+fulfilled.
+
+reject() adalah parameter kedua pada executor function. Parameter ini
+merupakan fungsi yang dapat menerima satu parameter dan digunakan untuk
+memberikan alasan kenapa Promise tidak dapat terpenuhi. Ketika fungsi ini
+terpanggil, kondisi Promise akan berubah dari pending menjadi rejected.
+Executor function akan berjalan secara asynchronous hingga akhirnya
+kondisi Promise berubah dari pending menjadi fulfilled/rejected.
+*/
