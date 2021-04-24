8afbd08f25473b6f979cbacaa79e836dbe134a17
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-04-24 12:59:28 +0800

    Update SetTime

---
+++ Concurrency/setTimeOut.js
@@ -0,0 +1,12 @@
+console.log("Selamat datang!");
+console.log("Ada yang bisa dibantu?");
+
+//WAKTU
+setTimeout(() => {
+  console.log("Terima kasih sudah mampir, silakan datang kembali!");
+}, 10000);
+
+//Waktu
+setTimeout(() => {
+  console.log("Mas? Cepat mas");
+}, 3000);
